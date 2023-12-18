97efb8b1803b461298aaad7b398ecb81e0b3894f
Michael Kolkov <55915846+mireabot@users.noreply.github.com>
2023-12-18 09:21:24 -0500

    Added info tip on application page

--- application.php
+++ application.php
@@ -73,6 +73,15 @@
             </div>
             <input type="submit" name="submit" id="submit" class="actionButton" value="Sign Up">
         </form>
+        <div class="flex items-center p-4 rounded-lg bg-gray-50" style="margin-top: 30px">
+            <svg class="flex-shrink-0 w-4 h-4 dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div class="ms-3 text-sm font-medium text-gray-800">
+                Have questions about program or club? Email <a href="mailto:mvatankhah@bmcc.cuny.edu" class="font-semibold underline hover:no-underline">Professor Vatankhah</a> for more information.
+            </div>
+        </div>
     </div>
 </section>
 <!-- FOOTER -->
